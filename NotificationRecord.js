3b66be80092a84d9f71e6289bc7119a615801cdc
DenizhanEne34 <107154954+DenizhanEne34@users.noreply.github.com>
2022-10-31 10:13:35 -0400

    Update NotificationRecord.js

--- NotificationRecord.js
+++ NotificationRecord.js
@@ -1,3 +1,10 @@
+//This is my code for creating a NotificationRecord model to track notification success in our weekly and 
+//monthly email and push notifications. My model had 6 fields: a user object field which holds a unique user token (to connect the nofitication to the user)
+//, a code field which holds the period of the Notification, it can be a weekly, monthly, or quarterly notifications, a createdAt field, a channel field which can hold 
+// either push or email. isSent is the key field on my model that allows me to check if a notification has been successfully sent or not, and the last
+//field is an error message field.
+
+
 const mongoose = require('mongoose');
 
 const NotificationRecordSchema = new mongoose.Schema({
