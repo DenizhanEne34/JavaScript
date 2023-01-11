cb7f27d15e7eb9c223b7601ba4546c465a89c375
DenizhanEne34 <107154954+DenizhanEne34@users.noreply.github.com>
2023-01-11 21:03:04 +0300

    Update mainCode.js

--- mainCode.js
+++ mainCode.js
@@ -191,3 +191,40 @@ const NotificationRecordSchema = new mongoose.Schema({
 
 module.exports = mongoose.model('NotificationRecord', NotificationRecordSchema);
 
+//This is my code for a receipt photo model used in our application.
+
+const mongoose = require('mongoose');
+
+const ReceiptPhotoSchema = new mongoose.Schema({
+  transaction: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'Transaction',
+    required: true,
+  },
+
+  thumbnail: {
+    type: String,
+    required: [true, 'Please add a base64 string for the thumbnail photo.'],
+  },
+
+  original: {
+    type: String,
+    required: [true, 'Please add a base64 string for the original photo.'],
+  },
+
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+ReceiptPhotoSchema.pre('save', async function (next) {
+  this.updatedAt = Date.now();
+});
+
+module.exports = mongoose.model('ReceiptPhoto', ReceiptPhotoSchema);
